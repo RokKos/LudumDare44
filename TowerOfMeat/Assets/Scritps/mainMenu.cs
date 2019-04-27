bc257348e3bbbcfa760c00808af0b3a5cc14db7b
Narieth <virtana@gmail.com>
2019-04-27 20:05:31 +0200

    Revert "Main Menu tweaks"
    This reverts commit 1f5696e91c748eea04f50ef2593ea2d40b2d70bc.

--- TowerOfMeat/Assets/Scritps/mainMenu.cs
+++ TowerOfMeat/Assets/Scritps/mainMenu.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    public GameObject board;
     // Start is called before the first frame update
     public void nextLevel(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,16 +14,4 @@ public class mainMenu : MonoBehaviour
         SceneManager.LoadScene("Main Menu");
     }
 
-    public void playGame(){
-        Vector3 boardPos = board.transform.position;
-        for(var i=0; i<100; i++){
-            boardPos.y += 1;
-            board.transform.position = boardPos;
-        }
-        for(var i=0;i<1000;i++){
-            Debug.Log(i);
-            boardPos.y -= 1;
-            board.transform.position = boardPos;
-        }
-    }
 }
