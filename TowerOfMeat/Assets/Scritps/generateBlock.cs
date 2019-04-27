0aaabca42e5a5861d7d9c101293198bae0e55ac1
Narieth <virtana@gmail.com>
2019-04-27 14:04:18 +0200

    Display next block

--- TowerOfMeat/Assets/Scritps/generateBlock.cs
+++ TowerOfMeat/Assets/Scritps/generateBlock.cs
@@ -5,30 +5,25 @@ using UnityEngine;
 public class generateBlock : MonoBehaviour
 {
     public List<MeatPartController> allBlocks;
+
+    private MeatPartController next;
+
     //public GameObject[] blockArray;
     // Start is called before the first frame update
     void Start()
     {
-        allBlocks = GameObject.FindGameObjectsWithTag("allBlocks");
-        Debug.Log(GameObject.FindGameObjectsWithTag("allBlocks").Length);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.Space)){
-            var randomBlock = Random.Range(0, allBlocks.Length);
-            Instantiate(allBlocks[randomBlock], new Vector3(0,5,0), Quaternion.identity);
-            Debug.Log(randomBlock);
-            //run through all blocks, just a test
-            for(int i=0;i<allBlocks.Length; i++){
-                
-                //Debug.Log(allBlocks[i]);
-            }
-        }
+        next =  Instantiate(allBlocks[Random.Range(0, allBlocks.Count)]);
+        next.transform.position = new Vector3(6.6f, 5.8f, 0);
+        next.EnableGravity(false);
+        
     }
 
     public MeatPartController GetBlock(){
-		return Instantiate(allBlocks[Random.Range(0, allBlocks.Count)]);
+        MeatPartController ret =  next;
+        next = Instantiate(allBlocks[Random.Range(0, allBlocks.Count)]);
+        next.transform.position = new Vector3(6.6f, 5.8f, 0);
+		next.EnableGravity(false);
+        return ret;
 	}
+    
 }
