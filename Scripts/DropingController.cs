7d410c317f9c806cf0f2b85c436e3a11fbf8a62a
Rok Kos <rok.kos@outfit7.com>
2019-04-27 11:56:24 +0200

    Repaired Project

---
+++ Scripts/DropingController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropingController : MonoBehaviour
+{
+	[SerializeField] MeatPartController meatPartPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			MeatPartController meatPart = Instantiate(meatPartPrefab, Vector3.zero, Quaternion.identity, transform);
+		}
+    }
+}
---
+++ TowerOfMeat/Assets/Scripts/MeatPartController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatPartController : MonoBehaviour
+{
+
+	[SerializeField] Rigidbody rigidbody;
+
+	private void OnCollisionEnter (Collision collision) {
+		rigidbody.velocity = Vector3.zero;
+	}
+}
