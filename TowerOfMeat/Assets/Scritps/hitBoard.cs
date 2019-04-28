24950f6a9b7cdcbb7519f09ea74e541829a51cbd
Narieth <virtana@gmail.com>
2019-04-28 09:31:20 +0200

    New  title board, added panel for next block

--- TowerOfMeat/Assets/Scritps/hitBoard.cs
+++ TowerOfMeat/Assets/Scritps/hitBoard.cs
@@ -6,13 +6,6 @@ public class hitBoard : MonoBehaviour
 {
     public float thrust;
     public Rigidbody board;
-    public GameObject cube;
-    private bool moveBoard = false;
-    private bool rotatingLeft = false;
-    private bool rotatingRight = false;
-    private int count = 0;
-    private int countLeft = 0;
-    private int countRight = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,55 +18,12 @@ public class hitBoard : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             Vector3 mousePos = Input.mousePosition;
-
             board.AddForceAtPosition(Vector3.forward * thrust,  Camera.main.ScreenToViewportPoint(mousePos) );
-            //Debug.Log(Camera.main.ScreenToViewportPoint(mousePos)[0]);
-            if(Camera.main.ScreenToViewportPoint(mousePos)[0]<0.4){
-                //Debug.Log("rotate left");
-                //rotatingLeft = true;
-                //board.transform.Rotate(0,1,0, Space.Self);
-            }
-            else if(Camera.main.ScreenToViewportPoint(mousePos)[0]>0.6){
-                //Debug.Log("rotate right");
-                //rotatingRight = true;
-                //board.transform.Rotate(0,-1,0, Space.Self);
-            }
-            
-            
-            //moveBoard = true;
         }
-        /*
-        if(rotatingLeft){
-            board.transform.Rotate(0,0.15f,0, Space.Self);
-            countLeft++;
-        }
-        else if(rotatingRight){
-            board.transform.Rotate(0,-0.15f,0, Space.Self);
-            countRight++;
-        }
-
-        if(countRight>=25){
-            countRight = 0;
-            rotatingRight = false;
-        }
-        if(countLeft>=25){
-            rotatingLeft = false;
-            countLeft = 0;
-        }
-        */
-        if(moveBoard){
-            //Debug.Log(moveBoard);
-            Vector3 cubePos = cube.transform.position;
-            
-            if(count>50 && count<100){
-                cubePos.y-=0.05f;
-            }
-            else if(count>=100){cubePos.y+=0.1f;}
-            count++;
-            cube.transform.position = cubePos;
-            if(cubePos.y>30){
-                moveBoard=false;
-            }
+        Vector3 boardPos = board.transform.position;
+        if(boardPos.x>6){
+            boardPos.x-=0.1f;
+            board.transform.position = boardPos;
         }
     }
 }
